945cbf2cf7dd5ff9178e66cf8905fdc3a1215a22
RGlossop <75027918+RGlossop@users.noreply.github.com>
2025-08-05 15:36:18 +0100

    [MTDSA-30833] increased code coverage

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -14,16 +14,13 @@
  * limitations under the License.
  */
 
-import play.core.PlayVersion
-import play.sbt.PlayImport.*
 import sbt.*
 
 object AppDependencies {
 
-  val bootstrapPlayVersion = "9.13.0"
+  val bootstrapPlayVersion = "9.19.0"
 
   val compile: Seq[ModuleID] = Seq(
-    ws,
     "uk.gov.hmrc"                  %% "bootstrap-backend-play-30" % bootstrapPlayVersion,
     "org.typelevel"                %% "cats-core"                 % "2.13.0",
     "com.fasterxml.jackson.module" %% "jackson-module-scala"      % "2.19.1",
@@ -32,8 +29,7 @@ object AppDependencies {
   )
 
   def test(scope: String = "test, it"): Seq[sbt.ModuleID] = Seq(
-    "org.scalatestplus"   %% "scalacheck-1-15"        % "3.2.11.0"           % scope,
-    "com.vladsch.flexmark" % "flexmark-all"           % "0.64.8"             % scope,
+    "org.scalatestplus"   %% "scalacheck-1-18"        % "3.2.19.0"           % scope,
     "org.scalamock"       %% "scalamock"              % "7.4.0"              % scope,
     "uk.gov.hmrc"         %% "bootstrap-test-play-30" % bootstrapPlayVersion % scope,
     "io.swagger.parser.v3" % "swagger-parser-v3"      % "2.1.31"             % scope
